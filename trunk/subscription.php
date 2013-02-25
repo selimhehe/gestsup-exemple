1dde07c0b404b60fd9a21ca375192f56c4d1faf0
mahmoud.nb@gmail.com <mahmoud.nb@gmail.com@bcfcaa32-9895-1782-41cf-77632ad76621>
2013-02-25 20:37:16 +0000

    mNbet : Ajouter la page 'subscription'

---
+++ trunk/subscription.php
@@ -0,0 +1,80 @@
+<?php
+/*
+Author: Florent Hauville
+File name: admin.php
+Description: admin parent page
+Version: 1.2
+creation date: 12/01/2011
+last update: 13/10/2012
+*/
+
+// initialize variables 
+if(!isset($_GET['subpage'])) $_GET['subpage'] = '';
+
+//default settings
+if ($_GET['subpage']=='') $_GET['subpage']='user';
+
+?>
+
+<h2 class="sec_head">Inscription</h2>
+<div id="catalogue">
+	<form id="1" name="form" method="post"  action="">
+		<table width="100%">
+			<tr>
+				 <th colspan="2" ><img alt="user" src="./images/user.png" sytle="border-style:none;"/>   Inscription utilisateur</th>
+			<tr>
+			<tr>
+				<td width="200"><label for="mail"><span class="required">*</span>Adresse mail:</label></td>
+				<td><input name="mail" id="mail" type="text" value="" size="20" /></td>
+			</tr>
+			<tr>
+				<td><label for="civilite"><span class="required">*</span>Civilité:</label></td>
+				<td>
+					
+					<input type="radio" name="civilite" id="civilite_m" value="M."  /> <label for="civilite_m">M.</label><br />
+					<input type="radio" name="civilite" id="civilite_mme" value="Mme"  /> <label for="civilite_mme">Mme</label><br />
+					<input type="radio" name="civilite" id="civilite_mlle" value="Mlle"  /> <label for="civilite_mlle">Mlle</label><br />
+					
+				</td>
+			</tr>
+			<tr>
+				<td><label for="nom"><span class="required">*</span>Nom:</label></td>
+				<td><input name="nom" id="nom" type="text" value="" size="20" /></td>
+			</tr>
+			<tr>
+				<td><label for="prenom"><span class="required">*</span>Prénom:</label></td>
+				<td><input name="prenom" id="prenom" type="text" value="" size="20" /></td>
+			</tr>
+			<tr>
+				<td><label for="fixe">N° de téléphone fixe:</label></td>
+				<td><input name="fixe" id="fixe" type="text" value="" size="20" /></td>
+			</tr>
+			<tr>
+				<td><label for="mobile"><span class="required">*</span>N° de mobile:</label></td>
+				<td><input name="mobile" id="mobile" type="text" value="" size="20" /></td>
+			</tr>
+			<tr>
+				<td><label for="ville"><span class="required">*</span>Ville:</label></td>
+				<td><input name="ville" id="ville" type="text" value="" size="20" /></td>
+			</tr>
+			<tr>
+				<td><label for="cp">Code postal:</label></td>
+				<td><input name="cp" id="cp" type="text" value="" size="20" /></td>
+			</tr>
+			<tr>
+				<td><label for="rue">N°, Rue:</label></td>
+				<td><input name="rue" id="rue" type="text" value="" size="20" /></td>
+			</tr>
+			<tr>
+				<td><label for="note">Note:</label></td>
+				<td><input name="note" id="note" type="text" value="" size="20" /></td>
+			</tr>
+			<tr>
+				<td><label for="tva"><span class="required">*</span>N° TVA:</label></td>
+				<td><input name="tva" id="tva" type="text" value="" size="20" /></td>
+			</tr>
+			
+
+		</table>
+	</form>
+</div>

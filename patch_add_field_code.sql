592b47474c5737f79364abfdc607f7767f5a9350
sahli28 <sahli28@gmail.com>
2013-02-26 22:42:16 +0000

    add subscription form

--- patch_add_field_code.sql
+++ patch_add_field_code.sql
@@ -1,3 +1,7 @@
 ALTER TABLE  `tusers` ADD  `code` VARCHAR( 100 )  NULL;
 ALTER TABLE  `tusers` ADD  `numero_rue` VARCHAR( 10 )  NULL;
-ALTER TABLE  `tusers` ADD  `service` VARCHAR( 200 ) NULL
+ALTER TABLE  `tusers` ADD  `service` VARCHAR( 200 ) NULL;
+ALTER TABLE  `tusers` ADD  `mobil` VARCHAR( 20 ) NULL;
+ALTER TABLE  `tusers` ADD  `civility` VARCHAR( 4 ) NULL;
+ALTER TABLE  `tusers` ADD  `code_tva` VARCHAR( 20 ) NULL ,
+ADD  `note` TEXT NULL;

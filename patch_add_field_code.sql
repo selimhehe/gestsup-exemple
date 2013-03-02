22ffa245fd42b2a1cd5b7595bdfe36d1fc1bdb53
sahli28 <sahli28@gmail.com>
2013-03-02 12:39:38 +0000

    add pict and field group_id

--- patch_add_field_code.sql
+++ patch_add_field_code.sql
@@ -5,3 +5,4 @@ ALTER TABLE  `tusers` ADD  `mobil` VARCHAR( 20 ) NULL;
 ALTER TABLE  `tusers` ADD  `civility` VARCHAR( 4 ) NULL;
 ALTER TABLE  `tusers` ADD  `code_tva` VARCHAR( 20 ) NULL ,
 ADD  `note` TEXT NULL;
+ALTER TABLE  `tusers` ADD  `group_id` INT NOT NULL COMMENT  'il pointe sur company'
